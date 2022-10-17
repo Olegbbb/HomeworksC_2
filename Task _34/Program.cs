f26df4c03f317f54baaeaacf49f3cd7e59311dd4
Oleg <apricot-ob@yandex.ru>
2022-10-17 17:37:06 +0300

    Задача34

---
+++ Task _34/Program.cs
@@ -0,0 +1,24 @@
+﻿void NewArray ( int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random(). Next(100,1000);
+    }
+}
+
+int AmountEvenNumbers (int[] array)
+{
+    int m = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 ==0)
+        m++;
+    }
+    return m;
+}
+
+Console.Clear();
+int[] ourArray = new int[10];
+NewArray (ourArray);
+Console.Write($" Наш массив [{string.Join(", ", ourArray)}]"); 
+Console.WriteLine($"  Количество четных чисел в числе равно: {AmountEvenNumbers(ourArray)}");
