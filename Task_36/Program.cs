4678199e5d829d6f9ba490be8cafb5b7300e2e86
Oleg <apricot-ob@yandex.ru>
2022-10-17 18:12:13 +0300

    Задача36

---
+++ Task_36/Program.cs
@@ -0,0 +1,23 @@
+﻿void NewArray ( int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random(). Next(-100,101);
+    }
+}
+
+int FindSum(int[] array)
+{
+    int m = 0;
+    for (int i = 1; i < array.Length; i+= 2)
+    {
+        m = m + array[i];
+    }
+    return m;
+}
+
+Console.Clear();
+int [] myarray = new int[10];
+NewArray ( myarray);
+Console.WriteLine($"Первоначальный массив [{string.Join(" ," ,myarray)}]");
+Console.WriteLine($"Сумма элементов на нечетных позициях равна: {FindSum(myarray)}");
