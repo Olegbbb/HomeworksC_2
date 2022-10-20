4af62bfaa0981cf57eec7b1379cd371012f0b01e
Oleg <apricot-ob@yandex.ru>
2022-10-20 22:17:15 +0300

    Задача41

---
+++ Task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число");
+        int m = Convert.ToInt32(Console.ReadLine());
+        array[i] = m;
+    }
+}
+int Counter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество чисел в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] NewArray = new int[n];
+FillArray(NewArray);
+Console.WriteLine($"Ваш массив: [{String.Join(" ,",NewArray )}]");
+Console.WriteLine("Количество чисел больше нуля равно: " + Counter(NewArray));
